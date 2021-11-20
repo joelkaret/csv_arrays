91ddb8699410ba2b70e6717294cd073454af51e8
joelkaret <81484847+joelkaret@users.noreply.github.com>
2021-11-20 22:27:12 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,22 @@
-import setuptools
+from setuptools import setup
 
+packages = [
+    'csv_arrays'
+]
 
-
-setuptools.setup(
-    name="csv_arrays",
-    version="0.0.1",
+setup(name="csv_arrays.py",
     author="Joel Karet",
     author_email="joelkaret@gmail.com",
-    description="Helpful maniuplation in regards to arrays stored in CSV files",
-    url="https://github.com/joelkaret/csv_arrays.py.git",
+    url="https://github.com/joelkaret/csv_arrays.py",
+    version="0.0.1",
+    packages=packages,
+    license='MIT'
+    description="Helpful maniuplation in regards to arrays stored in CSV files.",
+    #long_description = 
+    python_requires=">=3.6",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ],
-    package_dir={"": "csv_arrays"},
-    packages=setuptools.find_packages(where="csv_arrays"),
-    python_requires=">=3.6",
+    ]
 )
